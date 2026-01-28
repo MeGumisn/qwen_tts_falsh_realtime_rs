31c24c6f261de5b7403df1a7b079139040374f13
pw <592114387@qq.com>
2026-01-29 02:58:18 +0800

    [ADD]完善demo， 已经可以直接使用

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,126 @@
-mod qwen_tts_realtime;
-mod logging;
+use crate::qwen_tts_realtime::{AudioFormat, QwenTtsRealtimeCallback, prepare_qwen_tts_realtime};
+use base64::Engine;
+use std::fs::{File, OpenOptions, create_dir_all};
+use std::io::Write;
+use std::path::Path;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use tokio::sync::Mutex;
 
-fn main() {
-    println!("Hello, world!");
+mod logging;
+mod qwen_tts_realtime;
+
+struct MyCallback {
+    file: File,
+    session_finished: Arc<AtomicBool>,
+}
+
+impl MyCallback {
+    fn new(filename: &str, session_finished: Arc<AtomicBool>) -> Self {
+        let p = Path::new(filename);
+        if !p.exists() || !p.is_file() {
+            if let Some(parent) = p.parent() {
+                create_dir_all(parent).unwrap();
+            }
+        }
+        let file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(p)
+            .unwrap();
+        Self {
+            file,
+            session_finished,
+        }
+    }
+}
+
+impl QwenTtsRealtimeCallback for MyCallback {
+    fn on_open(&self) {
+        log::info!("Connection opened");
+    }
+
+    fn on_close(&self, close_msg: &str) {
+        log::info!("Connection closed: {}", close_msg);
+    }
+
+    fn on_finish(&mut self, close_msg: &str) {
+        log::info!("Session finished: {}", close_msg);
+        self.session_finished.store(true, Ordering::Relaxed);
+    }
+
+    fn on_event(&mut self, message: &str) -> bool {
+        log::info!("Received event: {}", message);
+        let v: serde_json::Value = serde_json::from_str(message).unwrap();
+        if let Some(event_type) = v.get("type") {
+            if let Some(event_type_str) = event_type.as_str() {
+                match event_type_str {
+                    "session.created" => {
+                        log::info!("event: session created");
+                    }
+                    "response.audio.delta" => {
+                        log::info!("event: response audio delta");
+                        if let Some(recv_audio_b64) = v.get("delta") {
+                            if let Some(recv_audio_b64_str) = recv_audio_b64.as_str() {
+                                let audio_bytes = base64::engine::general_purpose::STANDARD
+                                    .decode(recv_audio_b64_str)
+                                    .unwrap();
+                                self.file.write(&audio_bytes).unwrap();
+                            }
+                        }
+                    }
+                    "response.done" => {
+                        log::info!("event: response done");
+                    }
+                    "session.finished" => {
+                        log::info!("event: session finished");
+                        return true;
+                    }
+                    _ => {
+                        log::info!("unknown event type: {}", event_type_str);
+                    }
+                }
+            }
+        }
+        false
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let session_finished = Arc::new(AtomicBool::new(false));
+    let text_to_synthesize = [
+        "对吧~我就特别喜欢这种超市，",
+        "尤其是过年的时候",
+        "去逛超市",
+        "就会觉得",
+        "超级超级开心！",
+        "想买好多好多的东西呢。",
+    ];
+    let session_finished_arc_clone = Arc::clone(&session_finished);
+    let mut qwen_tts_realtime = prepare_qwen_tts_realtime(Some(Arc::new(Mutex::new(Box::new(
+        MyCallback::new("result_24k.pcm", session_finished_arc_clone),
+    )))))
+    .await;
+    let _ = qwen_tts_realtime
+        .update_session(
+            "Cherry",
+            AudioFormat::PCM_24000HZ_MONO_16BIT,
+            "server_commit",
+        )
+        .await;
+    for text in text_to_synthesize.iter() {
+        let _ = qwen_tts_realtime.append_text(text).await;
+    }
+    let _ = qwen_tts_realtime.finish().await;
+    loop {
+        tokio::select! {
+            _ = tokio::time::sleep(std::time::Duration::from_millis(100)) => {
+                if session_finished.load(Ordering::Relaxed) {
+                    println!("TTS 任務已自動完成。");
+                    break;
+                }
+            }
+        }
+    }
 }
--- src/qwen_tts_realtime.rs
+++ src/qwen_tts_realtime.rs
@@ -1,3 +1,4 @@
+use crate::logging::init_logger;
 use futures_util::stream::SplitSink;
 use futures_util::{SinkExt, StreamExt};
 use serde_json::json;
@@ -6,10 +7,10 @@ use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 use tokio_tungstenite::tungstenite::{Error, Message};
-use tokio_tungstenite::{MaybeTlsStream, WebSocketStream, connect_async};
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use uuid::Uuid;
 
-struct AudioFormat<'a> {
+pub struct AudioFormat<'a> {
     format: &'a str,
     sample_rate: u32,
     channels: &'a str,
@@ -33,7 +34,7 @@ impl<'a> AudioFormat<'a> {
             format_str: format_str,
         }
     }
-    const PCM_24000HZ_MONO_16BIT: Self = Self {
+    pub const PCM_24000HZ_MONO_16BIT: Self = Self {
         format: "pcm",
         sample_rate: 24000,
         channels: "mono",
@@ -42,20 +43,21 @@ impl<'a> AudioFormat<'a> {
     };
 }
 
-trait QwenTtsRealtimeCallback {
+pub trait QwenTtsRealtimeCallback {
     fn on_open(&self);
     fn on_close(&self, close_msg: &str);
+    fn on_finish(&mut self, close_msg: &str);
     fn on_event(&mut self, message: &str) -> bool;
 }
 
-struct QwenTtsRealtime {
+pub struct QwenTtsRealtime {
     stream_writer: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
 }
 
 impl QwenTtsRealtime {
     ///
     /// 与服务器建立连接，链接成功后需要update_session
-    async fn new(
+    pub async fn new(
         model_name: &str,
         api_key: &str,
         url: Option<&str>,
@@ -103,7 +105,7 @@ impl QwenTtsRealtime {
         if let Some(callback) = callback {
             callback.lock().await.as_ref().on_open();
             tokio::spawn(async move {
-                let callback_clone = callback.clone();
+                let callback_clone = Arc::clone(&callback);
                 while let Some(message) = stream_reader.next().await {
                     match message {
                         Ok(msg) => {
@@ -136,6 +138,11 @@ impl QwenTtsRealtime {
                     }
                 }
                 log::info!("reader task ended");
+                callback_clone
+                    .lock()
+                    .await
+                    .as_mut()
+                    .on_finish("reader task ended");
             });
         }
         Self { stream_writer }
@@ -146,7 +153,7 @@ impl QwenTtsRealtime {
     }
 
     /// 建立连接成功后，需要添加session conf
-    async fn update_session(
+    pub async fn update_session(
         &mut self,
         voice: &str,
         response_format: AudioFormat<'_>,
@@ -170,7 +177,7 @@ impl QwenTtsRealtime {
         Ok(())
     }
 
-    async fn append_text(&mut self, text: &str) -> Result<(), Error> {
+    pub async fn append_text(&mut self, text: &str) -> Result<(), Error> {
         let msg = json!({
                                 "event_id": self._generate_event_id(),
                     "type": "input_text_buffer.append",
@@ -182,7 +189,7 @@ impl QwenTtsRealtime {
         Ok(())
     }
 
-    async fn finish(&mut self) -> Result<(), Error> {
+    pub async fn finish(&mut self) -> Result<(), Error> {
         let msg = json!({
             "event_id": self._generate_event_id(),
             "type": "session.finish"
@@ -194,97 +201,26 @@ impl QwenTtsRealtime {
     }
 }
 
+pub async fn prepare_qwen_tts_realtime(
+    callback: Option<Arc<Mutex<Box<dyn QwenTtsRealtimeCallback + Sync + Send>>>>,
+) -> QwenTtsRealtime {
+    init_logger("info");
+    let api_key = std::env::var("DASHSCOPE_API_KEY").unwrap();
+    log::info!("{}", api_key);
+    QwenTtsRealtime::new(
+        "qwen3-tts-flash-realtime",
+        api_key.as_str(),
+        Some("wss://dashscope.aliyuncs.com/api-ws/v1/realtime"),
+        None,
+        callback,
+    )
+        .await
+}
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::logging::init_logger;
-    use base64::Engine;
-    use log::__private_api::log;
-    use std::fs::{File, OpenOptions, create_dir_all};
-    use std::io::Write;
-    use std::path::Path;
 
-    struct MyCallback {
-        file: File,
-    }
 
-    impl MyCallback {
-        fn new(filename: &str) -> Self {
-            let p = Path::new(filename);
-            if !p.exists() || !p.is_file() {
-                if let Some(parent) = p.parent() {
-                    create_dir_all(parent).unwrap();
-                }
-            }
-            let file = OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open(p)
-                .unwrap();
-            Self { file: file }
-        }
-    }
-    impl QwenTtsRealtimeCallback for MyCallback {
-        fn on_open(&self) {
-            log::info!("Connection opened");
-        }
-
-        fn on_close(&self, close_msg: &str) {
-            log::info!("Connection closed: {}", close_msg);
-        }
-
-        fn on_event(&mut self, message: &str) -> bool {
-            log::info!("Received event: {}", message);
-            let v: serde_json::Value = serde_json::from_str(message).unwrap();
-            if let Some(event_type) = v.get("type") {
-                if let Some(event_type_str) = event_type.as_str() {
-                    match event_type_str {
-                        "session.created" => {
-                            log::info!("event: session created");
-                        }
-                        "response.audio.delta" => {
-                            log::info!("event: response audio delta");
-                            if let Some(recv_audio_b64) = v.get("delta") {
-                                if let Some(recv_audio_b64_str) = recv_audio_b64.as_str() {
-                                    let audio_bytes = base64::engine::general_purpose::STANDARD
-                                        .decode(recv_audio_b64_str)
-                                        .unwrap();
-                                    self.file.write(&audio_bytes).unwrap();
-                                }
-                            }
-                        }
-                        "response.done" => {
-                            log::info!("event: response done");
-                        }
-                        "session.finished" => {
-                            log::info!("event: session finished");
-                            return true;
-                        }
-                        _ => {
-                            log::info!("unknown event type: {}", event_type_str);
-                        }
-                    }
-                }
-            }
-            false
-        }
-    }
-
-    async fn prepare_qwen_tts_realtime(
-        callback: Option<Arc<Mutex<Box<dyn QwenTtsRealtimeCallback + Sync + Send>>>>,
-    ) -> QwenTtsRealtime {
-        init_logger("info");
-        let api_key = std::env::var("DASHSCOPE_API_KEY").unwrap();
-        log::info!("{}", api_key);
-        QwenTtsRealtime::new(
-            "qwen3-tts-flash-realtime",
-            api_key.as_str(),
-            Some("wss://dashscope.aliyuncs.com/api-ws/v1/realtime"),
-            None,
-            callback,
-        )
-        .await
-    }
     #[tokio::test]
     async fn test_update_session() {
         let mut qwen_tts_realtime = prepare_qwen_tts_realtime(None).await;
@@ -313,36 +249,6 @@ mod tests {
             .append_text("你好，欢迎使用Qwen TTS实时语音合成服务。")
             .await;
 
-        tokio::signal::ctrl_c()
-            .await
-            .expect("Failed to listen for event");
-    }
-
-    #[tokio::test]
-    async fn test_tts_generate() {
-        let text_to_synthesize = [
-            "对吧~我就特别喜欢这种超市，",
-            "尤其是过年的时候",
-            "去逛超市",
-            "就会觉得",
-            "超级超级开心！",
-            "想买好多好多的东西呢。",
-        ];
-        let mut qwen_tts_realtime = prepare_qwen_tts_realtime(Some(Arc::new(Mutex::new(
-            Box::new(MyCallback::new("result_24k.pcm")),
-        ))))
-        .await;
-        let _ = qwen_tts_realtime
-            .update_session(
-                "Cherry",
-                AudioFormat::PCM_24000HZ_MONO_16BIT,
-                "server_commit",
-            )
-            .await;
-        for text in text_to_synthesize.iter() {
-            let _ = qwen_tts_realtime.append_text(text).await;
-        }
-        let _ = qwen_tts_realtime.finish().await;
         tokio::signal::ctrl_c()
             .await
             .expect("Failed to listen for event");
